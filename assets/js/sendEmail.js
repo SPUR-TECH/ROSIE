26963b6210b6ce772c05a56269d11e9235e3515c
Scott Atkinson <scott.atkin79@gmail.com>
2022-03-23 18:41:42 +0000

    fix email issue

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,12 +1,14 @@
 function sendMail(contactForm) {
-    emailjs.send("gmail", "rosie", {
+    emailjs.send("service_vncwzfa", "rosie", {
             "from_name": contactForm.name.value,
             "from_email": contactForm.emailaddress.value,
             "project_request": contactForm.projectsummary.value
         })
         .then(
             function (response) {
-                console.log("SUCCESS", response);
+                $("#success").text("Message Sent");
+                $("#success").css("background-color", "green");
+                $(".center-form form").get(0).reset();
             },
             function (error) {
                 console.log("FAILED", error);
